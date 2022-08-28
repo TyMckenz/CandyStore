6735cb105a92fa0da1a662decd038315ea6081e9
Tyler M <t_mckenzie@outlook.com>
2022-08-28 16:20:47 -0500

    Add project files.

---
+++ CandyStore/Controllers/StoreController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CandyStore.Models;
+
+namespace CandyStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        public List<Candy> candies;
+        public StoreController()
+        {
+            candies = new List<Candy>
+            {
+                new Candy
+                {
+                    Name = "M&M",
+                    Weight = 100.00,
+                    Price = 2.50
+                },
+                  new Candy
+                {
+                    Name = "Skittles",
+                    Weight = 120.00,
+                    Price = 2.50
+                },
+                    new Candy
+                {
+                    Name = "Starburst",
+                    Weight = 80.00,
+                    Price = 2.50
+                },
+                      new Candy
+                {
+                    Name = "Smarties",
+                    Weight = 120.00,
+                    Price = 2.50
+                }
+
+
+            };
+
+        }
+        public IActionResult Candies()
+        {
+            return View(candies);
+        }
+    }
+}
---
+++ CandyStore/Models/Candy.cs
@@ -0,0 +1,9 @@
+﻿namespace CandyStore.Models
+{
+    public class Candy
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public double Price { get; set; }
+    }
+}
